7cb4872dfbb74938343a79107162a311b37741a0
DESKTOP-RDGJIOL\poloz <polozhenskii@gmail.com>
2020-12-21 23:09:06 +0300

    Add incomes and out to the app

--- script.js
+++ script.js
@@ -82,8 +82,27 @@ const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance} €`;
 };
-
 calcDisplayBalance(account1.movements);
+
+calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int) => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 // Compute usernames
 const createUsernames = function (accs) {
@@ -95,7 +114,6 @@ const createUsernames = function (accs) {
       .join("");
   });
 };
-
 createUsernames(accounts);
 
 // TODO: implement login
