c1ad9640bc33625e36902ffc549f95e169f5bc4f
DESKTOP-RDGJIOL\poloz <polozhenskii@gmail.com>
2021-01-21 17:47:51 +0300

    Implement logout timer

--- script.js
+++ script.js
@@ -179,13 +179,42 @@ const updateUI = function (account) {
   calcDisplaySummary(account);
 };
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    // In each call print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = "Log in to get started";
+      containerApp.style.opacity = 0;
+    }
+
+    // Decrease 1s
+    time--;
+  };
+
+  // Set time to 5 minutes
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  return timer;
+};
+
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 // DBG: FAKE ALWAYS LOGGED IN
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
@@ -228,7 +257,13 @@ btnLogin.addEventListener("click", function (e) {
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = "";
+    // Remove keyboard focus
     inputLoginPin.blur();
+
+    // Timer
+    if (timer) clearInterval(timer);
+
+    timer = startLogOutTimer();
 
     updateUI(currentAccount);
   }
@@ -258,6 +293,10 @@ btnTransfer.addEventListener("click", function (e) {
     receiverAcc.movementsDates.push(new Date().toISOString());
 
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -280,6 +319,10 @@ btnLoan.addEventListener("click", function (e) {
 
       // Upadate UI
       updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 2500);
   }
   inputLoanAmount.value = "";
@@ -312,5 +355,3 @@ btnSort.addEventListener("click", function (e) {
   displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
-
-// TODO: Implement clock timer
